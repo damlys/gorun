947007c267229717446152efb7e4a93b563fd735
damlys <damian.lysiak@gmail.com>
2025-01-28 16:02:18 +0000

    iap-user-account

--- docs/iap/iap-service-account/curl.bash
+++ docs/iap/iap-service-account/curl.bash
@@ -9,6 +9,7 @@ function main {
   local client_id="764086219165-odk048stmh8m26dvs581a969ko87c6gh.apps.googleusercontent.com" # https://console.cloud.google.com/auth/clients/764086219165-odk048stmh8m26dvs581a969ko87c6gh.apps.googleusercontent.com?project=gogke-test-0
   local service_account="iap-accessor@gogke-test-0.iam.gserviceaccount.com"                  # https://console.cloud.google.com/iam-admin/serviceaccounts/details/106872722214711289330?project=gogke-test-0
   local request_url="https://kuard.gogke-test-7.damlys.pl/healthy"
+  local id_token
 
   id_token=$(
     gcloud auth print-identity-token \
