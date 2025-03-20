ef882866221e7cac331b435746fae70a4ef56824
damlys <damian.lysiak@gmail.com>
2025-03-20 12:44:44 +0000

    feature: handle many node pools

--- projects/core/terraform-modules/test/main.tf
+++ projects/core/terraform-modules/test/main.tf
@@ -1,5 +1,5 @@
 module "test_platform" {
-  source = "../../terraform-submodules/gke-platform" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/core/gke-platform/0.2.100.zip"
+  source = "../../terraform-submodules/gke-platform" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/core/gke-platform/0.2.104.zip"
 
   google_client_config = data.google_client_config.oauth2
   google_project       = data.google_project.this
@@ -7,7 +7,14 @@ module "test_platform" {
   platform_name   = "gogke-test-2"
   platform_domain = "gogke-test-2.damlys.dev"
 
-  node_spot_instances = true
+  node_pools = {
+    "spot-pool-1" = {
+      node_machine_type   = "n2d-standard-2"
+      node_spot_instances = true
+      node_min_instances  = 1
+      node_max_instances  = 1
+    }
+  }
 
   namespace_names = [
     "gomod-test-2",
