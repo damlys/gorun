3c226df6c45d9faa3434f757c6b6a5a9f218b2c7
damlys <damian.lysiak@gmail.com>
2025-03-28 07:53:13 +0000

    tests

--- projects/demo/go-modules/kuard-tester/test/http_test.go
+++ projects/demo/go-modules/kuard-tester/test/http_test.go
@@ -25,16 +25,19 @@ func TestSuccess(t *testing.T) {
 
 func TestRedirects(t *testing.T) {
 	testcases := [...]struct {
+		name        string
 		requestUrl  string
 		redirectUrl string
 		statusCode  int
 	}{
 		{
+			"http to https redirect",
 			"http://stateless-kuard.gogke-test-2.damlys.dev/-/env",
 			"https://stateless-kuard.gogke-test-2.damlys.dev/-/env",
 			http.StatusFound,
 		},
 		{
+			"old domain redirect",
 			"https://kuard.gogke-test-2.damlys.dev/-/env",
 			"https://stateless-kuard.gogke-test-2.damlys.dev/-/env",
 			http.StatusMovedPermanently,
@@ -42,29 +45,31 @@ func TestRedirects(t *testing.T) {
 	}
 
 	for _, tc := range testcases {
-		req, err := http.NewRequest("GET", tc.requestUrl, nil)
-		if err != nil {
-			t.Fatalf("request create error (%s): %v", tc.requestUrl, err)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", tc.requestUrl, nil)
+			if err != nil {
+				t.Fatalf("request create error (%s): %v", tc.requestUrl, err)
+			}
 
-		client := &http.Client{
-			// do not follow redirects
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			},
-		}
-		res, err := client.Do(req)
-		if err != nil {
-			t.Fatalf("request send error (%s): %v", tc.requestUrl, err)
-		}
-		defer res.Body.Close()
+			client := &http.Client{
+				// do not follow redirects
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+			}
+			res, err := client.Do(req)
+			if err != nil {
+				t.Fatalf("request send error (%s): %v", tc.requestUrl, err)
+			}
+			defer res.Body.Close()
 
-		if res.StatusCode != tc.statusCode {
-			t.Errorf("response status code (%s): %d, wanted: %d", tc.requestUrl, res.StatusCode, tc.statusCode)
-		}
+			if res.StatusCode != tc.statusCode {
+				t.Errorf("response status code (%s): %d, wanted: %d", tc.requestUrl, res.StatusCode, tc.statusCode)
+			}
 
-		if got := res.Header.Get("location"); got != tc.redirectUrl {
-			t.Errorf("response redirect URL (%s): %s, wanted: %s", tc.requestUrl, got, tc.redirectUrl)
-		}
+			if got := res.Header.Get("location"); got != tc.redirectUrl {
+				t.Errorf("response redirect URL (%s): %s, wanted: %s", tc.requestUrl, got, tc.redirectUrl)
+			}
+		})
 	}
 }
