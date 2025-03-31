a7b1197ffca04d1246c81402e15d52d85c402ca3
damlys <damian.lysiak@gmail.com>
2025-03-31 12:33:45 +0000

    changelog

--- scripts/lib/git.bash
+++ scripts/lib/git.bash
@@ -51,12 +51,5 @@ function git::commit {
   git restore --staged .
   git add "./${files_path}"
   git commit --message="${commit_message}"
-
-  if ! str::is_empty "${GITHUB_HEAD_REF}"; then
-    git push origin "${GITHUB_HEAD_REF}"
-  elif ! str::is_empty "${GITHUB_REF}"; then
-    git push origin "${GITHUB_REF}"
-  else
-    git push origin "$(git rev-parse --abbrev-ref HEAD)"
-  fi
+  git push origin HEAD
 }
