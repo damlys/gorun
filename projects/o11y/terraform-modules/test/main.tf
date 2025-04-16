70d2d4e39080a525913f56aab6918b3dae13f8d8
damlys <damian.lysiak@gmail.com>
2025-04-16 05:35:52 +0000

    chore: docs

--- projects/o11y/terraform-modules/test/main.tf
+++ projects/o11y/terraform-modules/test/main.tf
@@ -79,7 +79,7 @@ resource "kubernetes_cluster_role" "opentelemetry_targetallocator" {
 #######################################
 
 module "test_lgtm_stack" {
-  source = "../../terraform-submodules/gke-lgtm-stack" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/o11y/gke-lgtm-stack/0.3.100.zip"
+  source = "../../terraform-submodules/gke-lgtm-stack" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/o11y/gke-lgtm-stack/0.4.100.zip"
 
   google_project           = data.google_project.this
   google_container_cluster = data.google_container_cluster.this
@@ -89,7 +89,7 @@ module "test_lgtm_stack" {
 }
 
 module "test_otel_collectors" {
-  source = "../../terraform-submodules/k8s-otel-collectors" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/o11y/k8s-otel-collectors/0.3.100.zip"
+  source = "../../terraform-submodules/k8s-otel-collectors" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/o11y/k8s-otel-collectors/0.4.100.zip"
   depends_on = [
     helm_release.opentelemetry_operator,
   ]
@@ -100,7 +100,7 @@ module "test_otel_collectors" {
 }
 
 module "test_prom_exporters" {
-  source = "../../terraform-submodules/k8s-prom-exporters" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/o11y/k8s-prom-exporters/0.3.100.zip"
+  source = "../../terraform-submodules/k8s-prom-exporters" # "gcs::https://www.googleapis.com/storage/v1/gogcp-main-2-private-terraform-modules/gorun/o11y/k8s-prom-exporters/0.4.100.zip"
 
   blackbox_exporter_urls = [
     "https://grafana.gogke-test-2.damlys.dev/healthz",
