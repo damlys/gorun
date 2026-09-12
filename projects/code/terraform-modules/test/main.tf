3d872d56f163b1052a55877377b10f9778e25f4c
damlys <damian.lysiak@gmail.com>
2026-09-12 21:01:12 +0000

    workstation

--- projects/code/terraform-modules/test/main.tf
+++ projects/code/terraform-modules/test/main.tf
@@ -1,6 +1,6 @@
 module "damlys_test_workstation" {
-  # PROD source = "gcs::https://www.googleapis.com/storage/v1/gogcp-main-9-private-terraform-modules/gorun/code/k8s-workstation/0.9.100.zip"
-  source = "../../terraform-submodules/k8s-workstation"
+  # PROD source = "gcs::https://www.googleapis.com/storage/v1/gogcp-main-9-private-terraform-modules/gorun/core/k8s-workstation/0.9.100.zip"
+  source = "../../../core/terraform-submodules/k8s-workstation"
 
   domain = "damlys-test.gogke-test-9.damlys.pl"
 
